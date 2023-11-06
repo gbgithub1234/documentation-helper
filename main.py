e6985ba267227194eda1b14419af064585fd5ce4
gbgithub1234 <142700922+gbgithub1234@users.noreply.github.com>
2023-11-06 12:16:30 -0800

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ url2 = "https://www.pinecone.io/"
 
 multiline_str1 = """
 - created by Glen Brauer, Business Analyst in AAE (glenb@sfu.ca) \n
+
+- the problem: document-based information can be scattered in many places\n
+
+- the solution: provide a one-stop shopping resource for all document-based information\n
+
 - leverages ChatGPT and [Pinecone vector storage](%s) """ % url2
 
 multiline_str2 = """to access these [sample documents](%s)""" % url
